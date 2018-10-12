118333f20fed90dac5ddcdef6d26fc46ade71526
MIDN J.M. Hanling <m202430@usna.edu>
2018-10-12 08:22:32 -0700

    fixing hexify

--- tools/hexify.sh
+++ tools/hexify.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-readelf -x .text $1 | grep "0x" | cut -d " "  -f 4,5,6,7 | tr "\n" " " | sed "s/ //g" | sed -e "s/\(.\)\(.\)/\\\\x\\1\\2/g" ; echo
+bytes=$(readelf -x .text $1 | grep "0x080" | cut -d " " -f 4,5,6,7  | tr "\n" " " | sed "s/ //g" | sed "s/\.//g")
+echo $bytes | python -c "import sys; hex=sys.stdin.read().strip(); print ''.join('\\\\x%s%s'%(hex[i*2],hex[i*2+1]) for i in range(len(hex)/2))"
+
